bf292b6f47f58a9c28c41e55c72ab4ffedf903bd
SVGAnimate <76270254+SVGAnimate@users.noreply.github.com>
2021-05-03 22:39:11 +0200

    Create Main.php

---
+++ lib/Gtk/Main.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Gtk;
+
+//define('Gtk\ORIENTATION_HORIZONTAL', \GTK_ORIENTATION_HORIZONTAL);
+const ORIENTATION_HORIZONTAL = \GTK_ORIENTATION_HORIZONTAL;
+
+function Init(&$argc, &$argv) {
+    \gtk_init($argc, $argv);
+}
+
+function Main() {
+    \gtk_main();
+}
+
+function MainQuit() {
+    \gtk_main_quit();
+}
