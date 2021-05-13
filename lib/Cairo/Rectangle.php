c9c34387f797b02946b800608035838c63142c0a
SVGAnimate <glash.gnome@gmail.com>
2021-05-13 22:02:51 +0200

    Create Rectangle.php

---
+++ lib/Cairo/Rectangle.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Cairo graphics library
+ * @package cairo
+ */
+
+namespace Cairo;
+
+/**
+ * A data structure for holding a rectangle.
+ */
+class Rectangle extends \cairo_rectangle_t {
+}
