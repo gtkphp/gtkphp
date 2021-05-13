b572f8f946c441a508a1c7f1bae5c84fe74fec8b
SVGAnimate <glash.gnome@gmail.com>
2021-05-13 21:58:54 +0200

    Create Image.php

---
+++ lib/Cairo/Surface/Image.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Cairo\Surface;
+
+use Cairo\Surface;
+use Cairo\Format;
+
+class Image extends Surface {
+  /**
+   * Creates an image surface of the specified format and dimensions.
+   * @param Format $format
+   * @param int $width
+   * @param int $height
+   * @param int $stride
+   * @param mixed $data
+   */
+  static public function Create(Format $format, int $width, int $height, int $stride=0, $data=null):ImageSurface {
+    if (empty($data)) {
+      return \cairo_image_surface_create ($format, $width, $height);
+    } else {
+      return \cairo_image_surface_create_for_data ($data, $format, $width, $height, $stride);
+    }
+  }
+  
+}
