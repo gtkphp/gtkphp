c56785e5745514a089c27e71826011d42991f786
SVGAnimate <glash.gnome@gmail.com>
2021-05-13 21:48:54 +0200

    Create PdfSurface.php

---
+++ lib/Cairo/PdfSurface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Cairo;
+
+class PdfSurface extends Surface {
+  /**
+   * Creates a PDF surface of the specified size in points to be written to filename .
+   * @param string $filename
+   * @param float $width_in_points
+   * @param float $height_in_points
+   */
+  static public function Create(string $filename, float $width_in_points, float $height_in_points):Surface {
+    return \cairo_pdf_surface_create ($filename, $width_in_points, $height_in_points);
+  }
+}
