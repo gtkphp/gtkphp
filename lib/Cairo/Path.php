26d480be75c716ecf95022978edeb16aca2d52a2
SVGAnimate <glash.gnome@gmail.com>
2021-06-04 22:57:02 +0200

    Update Path.php

--- lib/Cairo/Path.php
+++ lib/Cairo/Path.php
@@ -10,6 +10,26 @@ namespace Cairo;
  *
  * $cr->appendPath($path);
  *
+ *
+ * for ($i=0; $i < $path->num_data; $i += $path->data[$i]->header->length) {
+ *    switch ($path->data->header->type) {
+ *    case CAIRO_PATH_MOVE_TO:
+ *        do_move_to_things ($path->data[$i+1]->point->x, $path->data[$i+1]->point->y);
+ *        break;
+ *    case CAIRO_PATH_LINE_TO:
+ *        do_line_to_things ($path->data[$i+1]->point->x, $path->data[$i+1]->point->y);
+ *        break;
+ *    case CAIRO_PATH_CURVE_TO:
+ *        do_curve_to_things ($path->data[$i+1]->point->x, $path->data[$i+1]->point->y,
+ *                            $path->data[$i+2]->point->x, $path->data[$i+2]->point->y,
+ *                            $path->data[$i+3]->point->x, $path->data[$i+3]->point->y);
+ *        break;
+ *    case CAIRO_PATH_CLOSE_PATH:
+ *        do_close_path_things ();
+ *        break;
+ *    }
+ * }
+ *
  */
 class Path extends \cairo_path_t {
     /*public function arc(float $xc, float $yc, float $radius, float $angle1, float $angle2) {
