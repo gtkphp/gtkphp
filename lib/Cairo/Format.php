4205d7fab3258b596f69021609c447eab2ac32df
SVGAnimate <glash.gnome@gmail.com>
2021-06-06 10:04:55 +0200

    Create Format.php

---
+++ lib/Cairo/Format.php
@@ -0,0 +1,13 @@
+<?php
+
+namepsace Cairo;
+
+class Format {
+  const INVALID = 0x00;
+  const ARGB32 = 0x01;
+  const RGB24 = 0x02;
+  const A8 = 0x03;
+  const A1 = 0x04;
+  const RGB16_565 = 0x05;
+  const RGB30 = 0x06;
+}
