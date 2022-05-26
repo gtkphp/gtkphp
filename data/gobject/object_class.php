b0d98ccb6210db9c0d9081955465d3bb8b9f4347
SVGAnimate <glash.gnome@gmail.com>
2022-05-26 13:19:03 +0200

    Update object_class.php

--- data/gobject/object_class.php
+++ data/gobject/object_class.php
@@ -1,5 +1,16 @@
 <?php
 
+see : https://github.com/gtkphp/gtkphp/discussions/2
+
 class GObjectClass extends GTypeClass
 {
+  static public function list_properties(object $o, int &$n_properties): GParamSpec[] {
+  }
 }
+
+/*
+$object = new GObject();
+$array = get_object_vars($object);
+
+GObjectClass::list_properties(GObject::class, $n_properties);
+*/
