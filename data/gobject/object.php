645f310aef321356d7ce8274b5d0c975d3683aae
SVGAnimate <glash.gnome@gmail.com>
2022-05-26 12:40:26 +0200

    Update object.php

--- data/gobject/object.php
+++ data/gobject/object.php
@@ -3,9 +3,14 @@
 
 class GObject
 {
-  
+  // opaque
 }
 
 
+/**
+ * Sets a property on an object.
+ * @param GObject $object a GObject
+ * @param string $property_name the name of the property to set
+ * @param GValue $value the value
+ */
 function g_object_set_property (GObject $object, string $property_name, GValue $value) {}
-
