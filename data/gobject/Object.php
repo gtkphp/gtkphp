50b4e3b6a3707dd14bd4245ffc2db199bb6c38b7
SVGAnimate <glash.gnome@gmail.com>
2022-05-27 08:15:43 +0200

    Update Object.php

--- data/gobject/Object.php
+++ data/gobject/Object.php
@@ -8,8 +8,6 @@ class GObject extends GType
   /* seldom overidden */
   public function __construct() {
     // Internal implementation
-    //g_object_class_install_property();
-    g_signal_new();
   }
   
   public function __destruct() {
@@ -25,10 +23,4 @@ class GObject extends GType
   /** @signal */
   public function __notify(GParamSpec $pspec) {
   }
-
-  /* seldom overidden */
-  public function dispatch_properties_changed(int	   $n_pspecs,
-					      GParamSpec[] $pspecs){
-  }
-
 }
