740465107f38e76af2ad524637626610cb01ddc4
SVGAnimate <glash.gnome@gmail.com>
2023-05-07 22:09:45 +0200

    Update SurfaceFactory.php

--- lib/Psr/Cairo/SurfaceFactory.php
+++ lib/Psr/Cairo/SurfaceFactory.php
@@ -5,8 +5,14 @@ namespace Psr\Cairo;
 use Psr\Cairo\Surface\PdfSurface;
 use Psr\Cairo\Surface\QuartzSurface;
 use Psr\Cairo\Format;
-  
+
 class SurfaceFactory {
+  public function createImage(Format $format,
+                            int $width,
+                            int $height):ImageSurface {}
+  public function createPng(Format $format,
+                            int $width,
+                            int $height):PngSurface {}
   public function createPdf(Format $format,
                             int $width,
                             int $height):PdfSurface {}
