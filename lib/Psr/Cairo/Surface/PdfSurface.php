f6212ef3a9b3837d875af1eaf91bce942b6ecf92
SVGAnimate <glash.gnome@gmail.com>
2023-05-07 22:42:45 +0200

    Update PdfSurface.php

--- lib/Psr/Cairo/Surface/PdfSurface.php
+++ lib/Psr/Cairo/Surface/PdfSurface.php
@@ -7,8 +7,8 @@ class PdfSurface extends AbstractSurface {
    * @internal cairo_pdf_surface_create
    */
   public function __construct(string $filename,
-                              double $width_in_points,
-                              double $height_in_points) {
+                              float $width_in_points,
+                              float $height_in_points) {
     parent::__construct(\cairo_pdf_surface_create($filename, $width_in_points, $height_in_points));
   }
 }
