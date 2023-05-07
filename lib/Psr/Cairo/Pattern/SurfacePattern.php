25a3a88ba72e7abb1685d19d15a1c20958502a95
SVGAnimate <glash.gnome@gmail.com>
2023-05-07 20:52:48 +0200

    Update SurfacePattern.php

--- lib/Psr/Cairo/Pattern/SurfacePattern.php
+++ lib/Psr/Cairo/Pattern/SurfacePattern.php
@@ -2,5 +2,10 @@
 
 namespace Psr\Cairo\Pattern;
 
+use Psr\Cairo\Surface\AbstractSurface;
+
 class SurfacePattern extends cairo_pattern_t {
+  public function getSurface():AbstractSurface {
+  }
+
 }
