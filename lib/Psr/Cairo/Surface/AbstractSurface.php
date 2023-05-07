5d28d4e329ef23647b21c7b0bf634d05c123268d
SVGAnimate <glash.gnome@gmail.com>
2023-05-07 22:46:53 +0200

    Update AbstractSurface.php

--- lib/Psr/Cairo/Surface/AbstractSurface.php
+++ lib/Psr/Cairo/Surface/AbstractSurface.php
@@ -4,4 +4,8 @@ namespace Psr\Cairo\Surface;
 
 class AbstractSurface extends \cairo_surface_t {
   protected function __construct(\cairo_surface_t $target) {}
+  /**
+   * @internal cairo_surface_show_page
+   */
+  protected function showPage() {}
 }
