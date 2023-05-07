363e72b73ca1c6d3d10cc64c2a394d743e999ff8
SVGAnimate <glash.gnome@gmail.com>
2023-05-07 20:51:21 +0200

    Update PatternFactory.php

--- lib/Psr/Cairo/PatternFactory.php
+++ lib/Psr/Cairo/PatternFactory.php
@@ -3,11 +3,21 @@
 namespace Psr\Cairo;
 
 use Psr\Cairo\Pattern\Type;
-use Psr\Cairo\Pattern\AbstractPattern;
+
+use Psr\Cairo\Pattern\SolidPattern;
+use Psr\Cairo\Pattern\SurfacePattern;
+use Psr\Cairo\Pattern\Gradient\LinearGradient;
+use Psr\Cairo\Pattern\Gradient\RadialGradient;
+use Psr\Cairo\Pattern\MeshPattern;
+
 
 class PatternFactory {
-  static public function Create(Type $type):AbstractPattern {
-  }
-  static public function Factory(Type $type):AbstractPattern {
-  }
+  public function createRgb ():SolidPattern {}
+  public function createRgba ():SolidPattern {}
+  public function createSurface ():SurfacePattern {}
+  public function createLinear ():LinearGradient {}
+  public function createRadial ():RadialGradient {}
+  public function createMesh ():MeshPattern {}
 }
+
+  
