27cab5c2474d19c0540704ed07a041bf447da27e
SVGAnimate <glash.gnome@gmail.com>
2023-05-07 20:28:46 +0200

    Create Pattern.php

---
+++ lib/Psr/Cairo/Pattern.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Psr\Cairo;
+
+use Psr\Cairo\Pattern\Type;
+use Psr\Cairo\Pattern\AbstractPattern;
+
+class Pattern {
+  static public function Create(Type $type):AbstractPattern {
+  }
+  static public function Factory(Type $type):AbstractPattern {
+  }
+}
