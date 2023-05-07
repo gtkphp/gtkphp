7767bb2eef562e3f078fc6e0f4224a5dbeb8e3ea
SVGAnimate <glash.gnome@gmail.com>
2023-05-07 21:46:21 +0200

    Create PngSurface.php

---
+++ lib/Psr/Cairo/Surface/PngSurface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Psr\Cairo\Surface;
+
+class PngSurface extends AbstractSurface {
+  /**
+   * @internal cairo_surface_write_to_png
+   * 
+   * @throws NoMemoryException if memory could not be allocated for the operation
+   * @throws MismatchSurfaceException if the surface does not have pixel contents
+   * @throws WriteErrorException if an I/O error occurs while attempting to write the file
+   * @throws PngErrorException if libpng returned an error
+   */
+  public function write(path $filename);
+}
