5dec9cb1c5fd6d4480bc9b9aead748a7e2513ce9
SVGAnimate <glash.gnome@gmail.com>
2023-05-07 18:10:55 +0200

    Update FreeType.php

--- lib/Cairo/FontFace/FreeType.php
+++ lib/Cairo/FontFace/FreeType.php
@@ -2,7 +2,13 @@
 
 namespace Cairo\FontFace;
 
+use Synthesize;
 
 class FreeType extends \cairo_font_face_t {
-  
+  public function getSynthesize():Synthesize {
+  }
+  public function setSynthesize(Synthesize $synth_flags) {
+  }
+  public function unsetSynthesize(Synthesize $synth_flags) {
+  }
 }
