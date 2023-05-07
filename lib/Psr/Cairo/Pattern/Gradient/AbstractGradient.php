6c9bdd7bd360f356720341998d642eef84f47ba1
SVGAnimate <glash.gnome@gmail.com>
2023-05-07 21:33:13 +0200

    Update AbstractGradient.php

--- lib/Psr/Cairo/Pattern/Gradient/AbstractGradient.php
+++ lib/Psr/Cairo/Pattern/Gradient/AbstractGradient.php
@@ -2,5 +2,23 @@
 
 namespace Psr\Cairo\Pattern\Gradient;
 
+use Psr\Cairo\Pattern\ColorStop;
+
 class AbstractGradient extends cairo_pattern_t {
+  /**
+   * @map cairo_pattern_add_color_stop_rgb
+   */
+  public function addColorStopRgb (double $offset,
+                                   double $red,
+                                   double $green,
+                                   double $blue);
+  /**
+   * @map cairo_pattern_add_color_stop_rgba
+   */
+  public function addColorStopRgb (double $offset,
+                                   double $red,
+                                   double $green,
+                                   double $blue,
+                                   double $alpha);
+  public function addColorStop (ColorStop $color_stop);
 }
