94df63eabdb53f548f526c8c1a2495121420e75d
SVGAnimate <glash.gnome@gmail.com>
2023-05-07 20:36:20 +0200

    Update PatternFactory.php

--- lib/Psr/Cairo/PatternFactory.php
+++ lib/Psr/Cairo/PatternFactory.php
@@ -1,1 +1,13 @@
+<?php
 
+namespace Psr\Cairo;
+
+use Psr\Cairo\Pattern\Type;
+use Psr\Cairo\Pattern\AbstractPattern;
+
+class PatternFactory {
+  static public function Create(Type $type):AbstractPattern {
+  }
+  static public function Factory(Type $type):AbstractPattern {
+  }
+}
