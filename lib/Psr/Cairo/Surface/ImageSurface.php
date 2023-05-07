2337d1baeb497632bf07972b87526b1526ce350a
SVGAnimate <glash.gnome@gmail.com>
2023-05-07 22:39:53 +0200

    Update ImageSurface.php

--- lib/Psr/Cairo/Surface/ImageSurface.php
+++ lib/Psr/Cairo/Surface/ImageSurface.php
@@ -3,5 +3,7 @@
 namespace Psr\Cairo\Surface;
 
 class ImageSurface extends AbstractSurface {
-  
+  public function __construct(Format $format, float $width, float $height) {
+    parent::__construct(\cairo_image_surface_create ($format->value, $width, $height));
+  }
 }
