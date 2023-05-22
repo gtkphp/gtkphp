588f9283ac7316ded45b6de3e905e743e44c8de4
SVGAnimate <glash.gnome@gmail.com>
2023-05-22 05:18:10 +0200

    Update Context.php

--- lib/Psr/Cairo/Context.php
+++ lib/Psr/Cairo/Context.php
@@ -4,6 +4,33 @@ namespace Psr\Cairo;
 
 use Psr\Cairo\PathTrait;
 
+/**
+ * @property Surface $target
+ * @property Pattern $source
+ */
 class Context extends \cairo_t {
   use PathTrait;
+
+  protected function __get($name) {
+    switch ($name) {
+      case 'target':
+        return cairo_get_target($this);
+      case 'source':
+        return cairo_get_source($this);
+      default:
+        // Error
+        break;
+    }
+  }
+  protected function __set($name, $value) {
+    switch ($name) {
+      case 'target':
+        // Readonly
+      case 'source':
+        return cairo_set_source($this, $value);
+      default:
+        // Error
+        break;
+    }
+  }
 }
